347a62e7f28419929be3469afb04335600708093
Paulius Kutka <kutka100@gmail.com>
2019-08-27 20:21:58 +0300

    Removed errors, added routing

--- src/products/services/toppings.service.ts
+++ src/products/services/toppings.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import "rxjs/add/observable/throw";
 import { Topping } from "../models/topping.model";
 
 @Injectable()
