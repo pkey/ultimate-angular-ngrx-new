3e0c81b1caa58701bc087fed33dede8d4668f536
Paulius Kutka <kutka100@gmail.com>
2019-09-04 08:14:42 +0300

    Updated actions with new syntax

--- src/store/actions/pizzas.actions.ts
+++ src/store/actions/pizzas.actions.ts
@@ -1,20 +1,12 @@
-import { Action } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 import { Pizza } from 'src/products/models/pizza.model';
 
-const LOAD_PIZZAS = '[Products] Load Pizzas';
-const LOAD_PIZZAS_SUCCESS = '[Products] Load Pizzas Success';
-const LOAD_PIZZAS_FAIL = '[Products] Load Pizzas Fail';
-
-export class LoadPizzas implements Action {
-  readonly type = LOAD_PIZZAS;
-}
-export class LoadPizzasSuccess implements Action {
-  readonly type = LOAD_PIZZAS_SUCCESS;
-  constructor(public payload: any) {}
-}
-export class LoadPizzasFail implements Action {
-  readonly type = LOAD_PIZZAS_FAIL;
-  constructor(public payload: Pizza[]) {}
-}
-
-export type PizzasAction = LoadPizzas | LoadPizzasFail | LoadPizzasSuccess;
+export const LoadPizzas = createAction('[Products] Load Pizzas');
+export const LoadPizzasSuccess = createAction(
+  '[Products] Load Pizzas Success',
+  props<{ payload: any }>()
+);
+export const LoadPizzasFail = createAction(
+  '[Products] Load Pizzas Fail',
+  props<{ payload: Pizza[] }>()
+);
